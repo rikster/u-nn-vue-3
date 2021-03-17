78e1224472b83c41e38e17f7b2eac1d75de60781
Richard Hounslow <rhounslow@gmail.com>
2021-03-17 14:49:45 +1100

    Updates to fetching data

---
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Job Details</h1>
+  <p>The job id is {{ id }}</p>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  // data() {
+  //   return {
+  //     id: this.$route.params.id
+  //   }
+  // }
+}
+</script>
+
+<style>
+
+</style>
